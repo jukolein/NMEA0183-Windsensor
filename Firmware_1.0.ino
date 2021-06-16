06077b59663bfe467f6499554603fbf08d5ae2f1
jukolein <julian.boehm@gmx.de>
2021-06-16 22:54:05 +0200

    Update Firmware_1.0.ino

--- Firmware_1.0.ino
+++ Firmware_1.0.ino
@@ -6,41 +6,41 @@
 #include <WiFiManager.h>         // https://github.com/tzapu/WiFiManager
 #include <Wire.h>
 #include <WiFiUdp.h>
-#include <AS5600.h>
+#include <AS5600.h>   //https://github.com/Seeed-Studio/Seeed_Arduino_AS5600
 
-//************************************************** ******
+
+//********************************************************
 // Global variables
-//************************************************** ******
+//********************************************************
+
 #define DELAY_MS 1000 // MS to wait
 #define DELAY_COUNT 1 // Counter of DELAY_MS to wait, 3600 is 1 hour
 #define PORT 8080 // Port to connect
-#define LED 2 // Built-in LED, on when broadcast
+#define LED 2 // Built-in LED
 
-AMS_5600 ams5600;
+AMS_5600 ams5600;  //select correct chip
 
+char CSSentence[30] = "" ; //string for sentence assembly
+char MWVSentence[30] = "" ; //string for MWV for sentence assembly (Wind Speed and Angle)
+char e[12] = "0";  //string for storing the angle
+char d[5] = "0";   //string for storing the wind speed before the dot
+char c[12] = "0";  //string for storing the wind speed after the dot
+volatile boolean blStatusChange = false;  //boolean for storing the current state of the hall sensor
+volatile long times = millis();  //used to calculate the time between two status changes
+long oldtimes = millis();   //used to calculate the time between two status changes
 
-char CSSentence[85] = "" ; //string for sentence assembly
-char MWVSentence[85] = "" ; //string for MWV for sentence assembly (Wind Speed and Angle)
-char e[50] = "0";
-char d[5] = "0";
-char c[50] = "0";
-volatile boolean blStatusChange = false;
-volatile long times = millis();
-long oldtimes = millis();
 
 //--------------------------------------------------------
 // ESP8266 WiFi variables
 //--------------------------------------------------------
+
 WiFiServer server(PORT); // Server/port
-IPAddress broadcastIP(255, 255, 255, 255);               // Senden an alle Netzwerkteilnehmer mit dieser Multicastadresse.
-WiFiUDP Udp;
-String header;
+IPAddress broadcastIP(255, 255, 255, 255); // use the broadcast IP to reach all possible listners
+WiFiUDP Udp;  //UPD protocol, because it is easy to handle and has less overhead than TCP
 
-
-
-//************************************************** ******
+//********************************************************
 // Support functions
-//************************************************** ******
+//********************************************************
 
 //--------------------------------------------------------
 // nmea_checksum - create NMEA checksum
@@ -48,30 +48,26 @@ String header;
 // $IIMDA,30.1,I,1.02,B,20.1,C,,C,,,,,,,,,,,,M*
 // $IIXDR,C,70.2,F,TempAir,P,1.03,B,Barometer*
 //--------------------------------------------------------
-String nmea_checksum(String s) {
-  char checksum = 0;
 
-  if ( s[0] == '$' && s[s.length() - 1] == '*') {
-    for (int i = 1; i < s.length() - 1; i++) {
-      checksum = checksum xor s[i];
-    }
-  }
-  return String(checksum, HEX);
-}
-
-String Checksum(char* CSSentence) {
-  int cs = 0; //clear any old checksum
-  char e[50] = "0";
+String checksum(char* CSSentence) {
+  int cs = 0; //stores the generated dezimal-checksum
+  char f[30] = "0";  //stores the final hex-checksum
   for (int n = 0; n < strlen(CSSentence); n++) {
     cs ^= CSSentence[n]; //calculates the checksum
   }
-  sprintf(e, "%02X", cs);
-  return e;
+  sprintf(f, "%02X", cs);  //convert the checksum into hex
+  return f;
 }
 
 
+//--------------------------------------------------------
+// WinSensInterupt - measure the time between the inerrupts
+// As this function runs whenever an interrupt is triggered,
+// it needs to have the "ICACHE_RAM_ATTR" at its beginning,
+// so it can run in RAM
+//--------------------------------------------------------
+
 ICACHE_RAM_ATTR void WinSensInterupt() {
-  Serial.print("I");
   if (times == 0) {
     oldtimes = millis();
     blStatusChange = false;
@@ -83,17 +79,18 @@ ICACHE_RAM_ATTR void WinSensInterupt() {
     blStatusChange = true;
     times = millis() - oldtimes;
     oldtimes = millis();
-    Serial.println(times);
   }
 }
 
-/*******************************************************
-  /* Function: convertRawAngleToDegrees
-  /* In: angle data from AMS_5600::getRawAngle
-  /* Out: human readable degrees as float
-  /* Description: takes the raw angle and calculates
-  /* float value in degrees.
-  /*******************************************************/
+
+//--------------------------------------------------------
+// Function: convertRawAngleToDegrees
+// In: angle data from AMS_5600::getRawAngle
+// Out: human readable degrees as float
+// Description: takes the raw angle and calculates
+// float value in degrees.
+//--------------------------------------------------------
+
 float convertRawAngleToDegrees(word newAngle) {
   /* Raw data reports 0 - 4095 segments, which is 0.087 of a degree */
   float retVal = newAngle * 0.087;
@@ -102,25 +99,33 @@ float convertRawAngleToDegrees(word newAngle) {
 }
 
 
-//************************************************** ******
+//********************************************************
 // Setup
-//************************************************** ******
+//********************************************************
 void setup() {
 
   Serial.begin(74880);         // Start the Serial communication to send messages to the computer
-  Wire.begin();
+  Wire.begin();  //Initiate the Wire library and join the I2C bus
   pinMode(LED, OUTPUT); // Initialize the LED pin as an output
-  pinMode(2, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);  //Initialize Pin 2 as Input and PullUp, to avoid floating states
+
+  Serial.print("begin Setup");
 
 
-  Serial.print("Versuche, zu verbinden");
+  //--------------------------------------------------------
+  // WiFiManager - takes care of the WiFi
+  // If the ESP can not connect to a known WiFi-Network,
+  // it opens an AP to connect to and set up a connection
+  // therefore, no physical access is requiered to change
+  // networks. Furthermore, it offers OTA FW-Updates.
+  //--------------------------------------------------------
 
   // WiFiManager
   // Local intialization. Once its business is done, there is no need to keep it around
   WiFiManager wifiManager;
 
   // Uncomment and run it once, if you want to erase all the stored information
-  wifiManager.resetSettings();
+  //wifiManager.resetSettings();
 
   // set custom ip for portal
   //wifiManager.setAPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
@@ -136,22 +141,17 @@ void setup() {
 
   server.begin();
 
-  Udp.begin(PORT);
-  /*
-    if(ams5600.detectMagnet() == 0 ){
-      while(1){
-          if(ams5600.detectMagnet() == 1 ){
-              Serial.print("Current Magnitude: ");
-              Serial.println(ams5600.getMagnitude());
-              break;
-          }
-          else{
-              Serial.print("Can not detect magnet");
-          }
-          delay(1000);
-      }
-    }
-  */
+  //--------------------------------------------------------
+  // Initialize UDP
+  //--------------------------------------------------------
+
+  Udp.begin(PORT); //Setup UDP
+
+
+  //--------------------------------------------------------
+  // ArduinoOTA - allows OTA-FW-flashing with the Arduino IDE
+  // must be included in every flash, or OTA-capability is lost
+  //--------------------------------------------------------
 
   ArduinoOTA.onStart([]() {
     String type;
@@ -194,58 +194,72 @@ void setup() {
   Serial.print("leave setup");
 }
 
-//************************************************** ******
+//********************************************************
 // Loop
-//************************************************** ******
+//********************************************************
+
 void loop() {
 
-  ArduinoOTA.handle();
+  ArduinoOTA.handle(); //deals with the ArduinoOTA-stuff, must be kept in to preserve posibility of OTA-FW-updates
 
-  int Z = convertRawAngleToDegrees(ams5600.getRawAngle());;
-  long WS = 0;
-  byte bySpeedCorrection = 100;
+  int Z = convertRawAngleToDegrees(ams5600.getRawAngle()); //stores the value of the angle
+  long WS = 0;  //stores the value of the wind speed
+  byte bySpeedCorrection = 100;  //value for finetuning the speed calculation
 
-  WS = ((33 * bySpeedCorrection) / (times));
+  WS = ((33 * bySpeedCorrection) / (times));  //calculating the wind speed
 
-  // Prüfsumme errechnen ******************************************
-  //Assemble a sentance of the various parts so that we can calculate the proper checksum NMEA
-  MWVSentence[0] = '\0';
-  strcpy(MWVSentence, "PIMWV,"); //The string could be kept the same as what it came in as, but I chose to use the P (prefix for all proprietary devices) and I for instrument
-  sprintf(e, "%d", Z);
+  //--------------------------------------------------------
+  // Assembly of the NMEA0183-sentence to calculate the checksum
+  // Currently,the programming is still somewhat inefficient,
+  // as the NMEA-sentence is created three times: for the
+  // checksum, the UDP and the SERIAL. This will be fixed
+  // in further releases.
+  //
+  // Beware: sprintf works on raw storage. If the chars
+  // are not initialized long enough, it will cause an
+  // buffer overflow
+  //--------------------------------------------------------
+
+  strcpy(MWVSentence, "WIMWV,");
+  sprintf(e, "%d", Z); //e represents the angle in degrees
   strcat(MWVSentence, e);
   strcat(MWVSentence, ",R,");
   sprintf(d, "%d", WS / 10); //d represent the meters/seconds
-  sprintf(c, ".%d", (WS % 10)); //c represent the deci knots
-
-  // sprintf(c, ".%d", (WS - ((WS / 10) * 10))); //c represent the deci knots
+  sprintf(c, ".%d", (WS % 10)); //c represent the decimal
   strcat(MWVSentence, d);
   strcat(MWVSentence, c);
   strcat(MWVSentence, ",M,A");
-  //******************************************************************
-  ///////////////////// NMEA to serial //////////////////////////
-  // NMEA(MWVSentence);
+
+  //--------------------------------------------------------
+  // Assembly and sending of the NMEA0183-sentence via UDP
+  //--------------------------------------------------------
 
   Udp.beginPacket(broadcastIP, PORT);
 
-  Udp.print("$WIMWV,"); // Assemble the final message and send it out the serial port
+  Udp.print("$WIMWV,");
   Udp.print(e);
   Udp.print(",R,");
   Udp.print(d);
   Udp.print(c);
   Udp.print(",M,A");
   Udp.print("*");
-  Udp.print(Checksum(MWVSentence)); // Call Checksum function
+  Udp.print(checksum(MWVSentence)); // Call Checksum function
 
   Udp.endPacket();
 
-  Serial.print("$WIMWV,"); // Assemble the final message and send it out the serial port
+
+  //--------------------------------------------------------
+  // Assembly and sending of the NMEA0183-sentence via SERIAL
+  //--------------------------------------------------------
+
+  Serial.print("$WIMWV,");
   Serial.print(e);
   Serial.print(",R,");
   Serial.print(d);
   Serial.print(c);
   Serial.print(",M,A");
   Serial.print("*");
-  Serial.print(Checksum(MWVSentence)); // Call Checksum function
+  Serial.print(checksum(MWVSentence)); // Call Checksum function
   Serial.print("\r");
   Serial.print("\n");
   Serial.println();
@@ -254,5 +268,4 @@ void loop() {
   for (int c = 0; c < DELAY_COUNT; c++) {
     delay(DELAY_MS);
   }
-
 }
